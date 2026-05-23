4f4bc64c34fdf609ed6d60ade9db8e11aed970e0
phenyomoletsane <moletsanephenyo232@gmail.com>
2026-05-23 17:15:15 +0000

    Deploying to gh-pages from @ phenyomoletsane/phenyomoletsane.github.io@856c6015b12f51cc59d2265527a1fc9083d6e12a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -490,30 +490,4 @@ ninja.data = [{
         handler: () => {
           window.open("https://www.linkedin.com/in/phenyo-moletsane-8254a542", "_blank");
         },
-      },{
-      id: 'light-theme',
-      title: 'Change theme to light',
-      description: 'Change the theme of the site to Light',
-      section: 'Theme',
-      handler: () => {
-        setThemeSetting("light");
-      },
-    },
-    {
-      id: 'dark-theme',
-      title: 'Change theme to dark',
-      description: 'Change the theme of the site to Dark',
-      section: 'Theme',
-      handler: () => {
-        setThemeSetting("dark");
-      },
-    },
-    {
-      id: 'system-theme',
-      title: 'Use system default theme',
-      description: 'Change the theme of the site to System Default',
-      section: 'Theme',
-      handler: () => {
-        setThemeSetting("system");
-      },
-    },];
+      },];
